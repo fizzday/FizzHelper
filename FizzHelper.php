cbef28f1be714155c48a6767091f7b75e59b2829
fizz <fizz_long@wochacha.com>
2016-08-19 11:41:05 +0800

    helper init

---
+++ FizzHelper.php
@@ -0,0 +1,62 @@
+<?php
+
+if (!function_exists('v')) {
+    /**
+     * 格式化打印, 不终止
+     * @param string $data
+     */
+    function v($data = '')
+    {
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    }
+}
+
+if (!function_exists('d')) {
+    /**
+     * 格式化打印, 并终止
+     * @param string $data
+     */
+    function d($data = '')
+    {
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+        die;
+    }
+}
+
+if (!function_exists('returnTrue')) {
+    /**
+     * 类的方法返回数据(成功返回)
+     * @param array $data
+     * @return array
+     */
+    function returnTrue($data = [])
+    {
+        $data['status'] = 1;
+        $data['data'] = $data;
+        $data['msg'] = 'success';
+
+        return $data;
+    }
+}
+
+if (!function_exists('returnFalse')) {
+    /**
+     * 类的方法返回数据(失败返回)
+     * @param array $data
+     * @return array
+     */
+    function returnTrue($data = [])
+    {
+        $data['status'] = 0;
+        $data['data'] = $data;
+        $data['msg'] = 'fail';
+
+        return $data;
+    }
+}
+
+
---
+++ demo/test.php
@@ -0,0 +1,38 @@
+<?php
+
+require __DIR__."/../vendor/autoload.php";
+
+$a = ['aa', 'bb'];
+$b = ['cc', 'dd'];
+
+v(array_merge($a,$b));
+v($a);
+
+$a = '
+IQLEEE’?
+
+RI; *0?!
+:I:l98392-14 3‘-
+n2nn%5I=;‘RT-1iiiilﬁ[Z%\= -.~
+
+M m
+
+ﬂklhﬁi 2321031 9830214  487 X
+
+
+';
+
+preg_match('/[\d]{14,17}[\d|X]{1}/', str_replace(' ', '', $a), $matches);
+
+v($matches);
+
+$arr = [
+    'a'=>1,
+    'b'=>2,
+    'c'=>[
+        'd'=>3,
+        'e'=>4
+    ]
+];
+v($arr);
+d($arr);
